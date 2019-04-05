cbaac647b61fcd8fe8e4149d5da0b44208c48402
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-04-06 01:41:51 +0200

    Updated since it's dependant on other files.

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <!-- https://www.canva.com/learn/wp-content/uploads/2016/01/Palette_20-tb-800x0.jpg   (color scheme to be implemented.)-->
 
+<!-- 
+	buglist:
+
+	popraviti problem sa ucitavanjem usericon.png kod index.php zbog neprilagodjenog pristupa parent-directoryu.
+
+-->
+
 <?php 
 	
+	include( "/sql/sql_config.php" );
 	include( "/user/session.php" );
-
 ?>
 
 <html>
@@ -22,63 +29,13 @@
 	<title>CRUD - Home</title>
 <body>
 
-	<nav class="navbar navbar-expand-sm navbar-dark rounded-bottom" id="nav_id">
-	  <!-- account icon -->
-	  <a class="navbar-brand" href="login/login.php">
-	  	<?php
-	  	/*
-	  		$query = "SELECT userPhoto FROM Users WHERE userid = $userid LIMIT 1;";
-	  		if( mysqli_query( $conn, $query ) ) {
-
-	  		} 
-		*/
-	  	?>
-	    <img src="resources/usericon.png" alt="logo" id="usericonid">
-	  </a>
-	  
-	  <!-- listitems -->
-	  <ul class="navbar-nav">
-	    <li class="nav-item">
-	      <a class="nav-link nav_listitems" href="">Home</a>
-	    </li>
-	    <li class="nav-item dropdown">
-	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">User</a>
-	        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-	        	<a class="dropdown-item" href="#">Dashboard</a>
-	        	<a class="dropdown-item" href="#">Settings</a>
-	        	<a class="dropdown-item" href="#">Privacy</a>
-	        </div>
-	    </li>
-	    <li class="nav-item">
-	      <a class="nav-link" href="#">About us</a>
-	    </li>
-	    <li class="nav-item">
-	      <a class="nav-link" href="#">Features</a>
-	    </li>
-	  </ul>
-	</nav>
-
-<?php
-	/*
-		todo list:
-
-		navbar (buttons and layout)
-		body of page
-		buttons on navbar for login and register
-		maybe instead of that logo that's places on the top right corner(navbar), i might use user's avatar which can be gathered by requests
-		roles (user, supervisor, admin)
-		mysql database
-		protect queries of possible sql injection attacks
-		inputfield spam protection (bruteforce maybe?)
-		hash and salt sensible user data to provide maximum security.
-		several options which do represent all 4 parts of CRUD
-		admin panel
-		security tokens for login sessions
-		anti-flood protection
-		a good looking footer
-
-	*/
-?>
+	<!-- Navbar -->
+	<script>
+		$( function() {
+      		$( "#navbar_include" ).load( "navbar.php" ); 
+    	} );
+	</script>
+	<div id="navbar_include"></div>
 
 </body>
 </html>
