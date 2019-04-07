645b3c09818c3a7e46bb0f5aca5a9bc094e9beaa
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-04-07 19:14:34 +0200

    Refactorization

--- register/register.php
+++ register/register.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-
-
-
+	<script src="../external.js"></script>
+	<script src="/register_errors.js"></script>
 <?php 
 
 	include( "../sql/sql_config.php" );
-	include( "../regex.php" ); // patterns and pattern validation
+	include( "../regex/regex.php" ); // patterns and pattern validation
 	include( "../user/session.php" );
 	check_session( "../index.php" );
 
@@ -15,28 +14,28 @@
 		$temp_password = mysqli_real_escape_string( $conn, $_POST[ 'password' ] );
 		$temp_mail = mysqli_real_escape_string( $conn, $_POST[ 'mail' ] );
 
-		// RegEx
-	    if( regex_validate( regex_mail_pattern, $temp_mail ) ) return header( "location: /register.php?error=mail" );
-	    if( regex_validate( regex_pass_pattern, $temp_password ) ) return header( "location: /register.php?error=password" );
-	    if( regex_validate( regex_user_pattern, $temp_username ) ) return header( "location: /register.php?error=username" );
+		// RegEx validation
+	    if( regex_validate( regex_mail_pattern, $temp_mail ) ) return header( "location: /register/register.php?error=mail" );
+	    if( regex_validate( regex_pass_pattern, $temp_password ) ) return header( "location: /register/register.php?error=password" );
+	    if( regex_validate( regex_user_pattern, $temp_username ) ) return header( "location: /register/register.php?error=username" );
 
 	    // Length check
-	    if( strlen( $temp_password ) < 5 || strlen( $temp_password ) > 20 ) return header( "location: /register.php?error=password" );
-	    if( strlen( $temp_username ) < 3 || strlen( $temp_username ) > 24 ) return header( "location: /register.php?error=username" );
+	    if( strlen( $temp_password ) < 5 || strlen( $temp_password ) > 20 ) return header( "location: /register/register.php?error=password" );
+	    if( strlen( $temp_username ) < 3 || strlen( $temp_username ) > 24 ) return header( "location: /register/register.php?error=username" );
 
 		$query = "SELECT * FROM crud_database.Users WHERE Username = '$temp_username' OR Mail = '$temp_mail'";
 		$resultset = mysqli_query( $conn, $query );
 	    $row = mysqli_fetch_array( $resultset, MYSQLI_ASSOC );
 	    $count = mysqli_num_rows( $resultset );
 	    if( $count ) {
-	    	header( "location: ../register/register.php?error=exists" ); 
+	    	header( "location: ../register/register.php?error=exists" ); die();
 	    } else {
 
 			$query = "INSERT INTO crud_database.Users( `Username`, `Password`, `Mail` ) VALUES ( '$temp_username', '$temp_password', '$temp_mail' )";
 			mysqli_query( $conn, $query );
 
 			$_SESSION[ 'session_username' ] = $temp_username;
-			header( "location: ../index.php?registration=complete" );
+			header( "location: ../home/index.php?registration=complete" ); die();
 		}
 	}
 
@@ -62,10 +61,14 @@
 	<!-- Navbar -->
 	<script>
 		$( function() {
-      		$( "#navbar_include" ).load( "../navbar.php" ); 
+      		$( "#navbar_include" ).load( "../external/navbar.php" ); 
+    	} );
+		$( function() {
+      		$( "#footer_container" ).load( "../external/footer.php" );
     	} );
 	</script>
 	<div id="navbar_include"></div>
+	<div id="footer_container"></div>
 	<div id="mainbox">
 		Create a New Account
 		<form action="" method="POST">
@@ -77,5 +80,7 @@
 	</div>
 	<div id="notice" style="font-size: 12px;">By clicking 'Sign Up', you are accepting our <a href="../tos.php" id="tos_props">ToS</a> and <a href="../user/privacy.php" id="tos_props">PP</span>.</div>
 	<div id="login">Already have an account? Proceed to login.</div>
+	<div id="snackbar"></div>
 </body>
+	<script>displayError();</script>
 </html>
