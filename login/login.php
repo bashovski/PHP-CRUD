d675a64b4d6914b920220a54d903dd15d8f8481a
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-04-11 23:53:08 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 
-	<script src="../external.js"></script>
+	<script src="../external/external.js"></script>
 	<script src="../login/url_parameters.js"></script>
 <?php
 
 	include( "../sql/sql_config.php" );
 	include( "../user/session.php" );
 
-	check_session( "../index.php" );
+	check_session( "../home/index.php" );
 
 	// Internal input handling
 	if( $_SERVER[ "REQUEST_METHOD" ] == "POST" ) {
 
-		$temp_username = mysqli_real_escape_string( $conn, $_POST[ 'username' ] );
-		$temp_password = mysqli_real_escape_string( $conn, $_POST[ 'password' ] );
+		$temp_username = mysqli_real_escape_string( $GLOBALS[ 'conn' ], $_POST[ 'username' ] );
+		$temp_password = mysqli_real_escape_string( $GLOBALS[ 'conn' ], $_POST[ 'password' ] );
 
 		$query = "SELECT * FROM crud_database.Users WHERE Username = '$temp_username' AND Password = '$temp_password' LIMIT 1;";
-		$resultset = mysqli_query( $conn, $query );
+		$resultset = mysqli_query( $GLOBALS[ 'conn' ], $query );
 	    $row = mysqli_fetch_array( $resultset, MYSQLI_ASSOC );
 	    $count = mysqli_num_rows( $resultset );
 		if( $count ) {
 			$_SESSION[ 'session_username' ] = $temp_username;
 			//print( "<script>console.log( 'Logged in' );</script>");
-			header( "location: ../index.php" );
+			header( "location: ../home/index.php" ); die();
 		} else {
 			header( "location: ../login/login.php?name=$temp_username&type=failed" );
-			print( "<script>updateLoginPage();</script>" );
+			print( "<script>updateLoginPage();</script>" ); 
 		}
 	}
 ?>
@@ -50,10 +50,14 @@
 	<!-- Navbar -->
 	<script>
 		$( function(){
-      		$( "#navbar_include" ).load( "../navbar.php" ); 
+      		$( "#navbar_include" ).load( "../external/navbar.php" ); 
+    	} );
+		$( function() {
+      		$( "#footer_container" ).load( "../external/footer.php" );
     	} );
 	</script>
 	<div id="navbar_include"></div>
+	<div id="footer_container"></div>
 	
 	<!-- main rect container -->
 	<div id="mainbox">
