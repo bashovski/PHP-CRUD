70690d4b3b9ebb84abb5f10c400011b9cab21f89
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-04-14 23:34:51 +0200

    test

--- sql/sql_groups.php
+++ sql/sql_groups.php
@@ -4,7 +4,7 @@
 		$query = "SELECT * FROM crud_database.Groups WHERE GroupActive IS NOT NULL";
 		$resultset = mysqli_query( $GLOBALS[ 'conn' ], $query );
 		$numrows = mysqli_num_rows( $resultset );
-		print( '<div style="text-align: center;" id="groups_num">'.$numrows. ' groups created</div>' );
+		print( '<div style="text-align: center;" id="groups_num">'.$numrows. ' groups created</div>' ); //test
 	}
 
 ?>
